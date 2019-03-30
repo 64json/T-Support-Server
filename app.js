0737ad3e7d48e833957bf49b91a8793de0c9abcd
Jason Park <parkjs814@gmail.com>
2019-03-30 06:59:20 -0400

    Hi

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const transcriptsRouter = require('./routes/transcripts');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/transcripts', transcriptsRouter);
 
 module.exports = app;
---
+++ routes/transcripts.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+let lastTranscripts = null;
+
+router.post('/', (req, res, next) => {
+  const { results } = JSON.parse(req.body.AddOns);
+  const { content_type, url } = results.voicebase_transcription.payload[0];
+  axios.get(url, {
+    headers: {
+      'Content-Type': content_type,
+    },
+    auth: {
+      username: accountSid,
+      password: authToken,
+    },
+  })
+    .then(response => {
+      const { text } = response.data.media.transcripts;
+      const firstSpeaker = text.startsWith('Speaker 1: ') ? 1 : 2;
+      const conversations = text.split(/\s*Speaker \d: /).slice(1);
+      lastTranscripts = { firstSpeaker, conversations };
+    })
+    .then(() => client.messages.create({
+      body: 'Thank you for contacting our T-Mobile representative. Please take time to rate your experience: https://jasonpark.me/T-Support/#/survey',
+      from: '+18138920179',
+      to: '+14703341764',
+    }))
+    .then(() => res.json({ success: true }))
+    .catch(next);
+});
+
+router.get('/', (req, res) => res.json({ transcripts: lastTranscripts }));
+
+module.exports = router;
