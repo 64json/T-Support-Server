c1b9e0b5ffd6dc767585e6adbbcbe994bf7f031d
Jason Park <parkjs814@gmail.com>
2019-03-30 05:01:56 -0400

    Test

--- routes/index.js
+++ routes/index.js
@@ -4,54 +4,12 @@ const request = require('request');
 const router = express.Router();
 const fs = require('fs');
 
-let question = '';
-let solution = '';
+let transcript = '';
 
-let download = function (url, dest, cb) {
-  let file = fs.createWriteStream(dest);
-  https.get(url, response => {
-    response.pipe(file);
-    file.on('finish', function () {
-      file.close(cb);  // close() is async, call cb after close completes.
-    });
-  }).on('error', function (err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
-  });
-};
-
-router.post('/question', (req, res) => {
-  question = req.body.text;
-
-  res.json({ success: true });
+router.post('/transcript', (req, res, next) => {
+  console.log(req.body);
 });
 
-router.post('/answer', (req, res, next) => {
-  const { url } = req.body;
-  download(`${url}.mp3`, 'answer.mp3', () => {
-    request.post({
-      headers: { 'content-type': 'audio/mp3' },
-      url: 'https://gateway-wdc.watsonplatform.net/speech-to-text/api/v1/recognize',
-      body: fs.createReadStream('answer.mp3'),
-      encoding: null,
-      auth: {
-        'user': 'apikey',
-        'pass': 'Wk54vkGwPI8FlZAckH9j9KbNDtJ1dMwdqekaPk5_HxK_',
-      },
-    }, (error, response, body) => {
-      solution = '';
-      if (error) return next(error);
-      let { results } = JSON.parse(body);
-      for (const result of results) {
-        solution += result.alternatives[0].transcript;
-      }
-
-      res.json({ success: true });
-    });
-
-  });
-});
-
-router.get('/get', (req, res) => res.json({ question, solution }));
+router.get('/transcript', (req, res) => res.json({ transcript }));
 
 module.exports = router;
